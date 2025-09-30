c4c8ff449bc46ceb210756175326429ba83a8973
JashithSharma <jashith.24037@stu.upes.ac.in>
2025-09-30 16:40:29 +0530

    Create 01_print_details.c

---
+++ 01_print_details.c
@@ -0,0 +1,9 @@
+// Q1 WAP to print your name, roll number, and address
+#include <stdio.h>
+
+int main() {
+    printf("name: Jashith Sharma\n");
+    printf("roll no: 12345\n);
+    printf("address: xyz\n");
+
+    return 0;
